8538c8bd54f3ec115f509d65a12a77e6dd6eef81
kikim1919 <kikim191957@gmail.com>
2025-12-09 11:59:40 +0900

    Update MessageRepository.java

--- heartlink/src/main/java/com/helena/heartlink/MessageRepository.java
+++ heartlink/src/main/java/com/helena/heartlink/MessageRepository.java
@@ -2,8 +2,6 @@ package com.helena.heartlink;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// <Message, Long> 뜻: "Message라는 요리를 저장할 거고, 번호표는 Long(숫자) 타입이야."
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    // 놀랍게도... 아무것도 안 적어도 저장, 조회, 삭제 기능이 자동으로 생겨!
-    // 이게 바로 스프링부트의 마법이지!
+    // 아무것도 안 적어도 저장, 조회, 삭제 기능이 자동으로 생김!
 }
