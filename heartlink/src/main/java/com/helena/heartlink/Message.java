447180f098aa947f3ed2d1d3462c5a76025df7c8
kikim1919 <kikim191957@gmail.com>
2025-12-09 11:57:31 +0900

    Update Message.java

--- heartlink/src/main/java/com/helena/heartlink/Message.java
+++ heartlink/src/main/java/com/helena/heartlink/Message.java
@@ -5,18 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity // "이건 데이터베이스에 저장할 녀석이야!" 라고 도장 쾅!
-@Getter // "이 안에 든 내용을 꺼내 볼 수 있게 해줘!"
-@Setter // "이 안에 내용을 수정할 수 있게 해줘!"
-@NoArgsConstructor // "빈 종이도 일단 만들어줘!"
+@Entity // 데이터 베이스에 저장
+@Getter // 내용 열람
+@Setter // 내용 수정
+@NoArgsConstructor // 빈 내용 허가
 public class Message {
 
     @Id // "이건 주민등록번호 같은 고유 번호야."
     @GeneratedValue(strategy = GenerationType.IDENTITY) // "번호는 1, 2, 3... 자동으로 매겨줘."
     private Long id;
 
-    private String nickname; // 작성자 닉네임 (예: 헬레나)
-    private String content;  // 하고 싶은 말 (예: 너 천재야!)
+    private String nickname; // 작성자 닉네임
+    private String content;  // 하고 싶은 말
     private String password; // 삭제할 때 쓸 비밀번호
 
     // 생성자: 처음에 글을 쓸 때 필요한 것들을 묶어주는 역할
