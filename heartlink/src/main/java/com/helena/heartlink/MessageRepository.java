60f16c9867531870f678cd7bb1b2ea8f46317770
kikim1919 <kikim191957@gmail.com>
2025-12-09 11:59:49 +0900

    Update MessageRepository.java

--- heartlink/src/main/java/com/helena/heartlink/MessageRepository.java
+++ heartlink/src/main/java/com/helena/heartlink/MessageRepository.java
@@ -3,5 +3,5 @@ package com.helena.heartlink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    // 아무것도 안 적어도 저장, 조회, 삭제 기능이 자동으로 생김!
+    // 아무것도 안 적어도 저장, 조회, 삭제 기능이 자동으로 생김
 }
